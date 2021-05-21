71ccf262bdb27a2b96012527e9bc27b9ae2d3c74
desexcile <34170928+desexcile@users.noreply.github.com>
2021-05-21 21:32:11 +0300

    added 1st lesson

---
+++ Lesson1_Borisenko/excile_framework/main.py
@@ -0,0 +1,54 @@
+import quopri
+from urllib.parse import parse_qsl
+
+
+class PageNotFound:
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE Not Found'
+
+
+class Framework:
+
+    """Класс Framework - основа фреймворка"""
+
+    def __init__(self, routes_obj, fronts_obj):
+        self.routes_lst = routes_obj
+        self.fronts_lst = fronts_obj
+
+    def __call__(self, environ, start_response):
+        # получаем адрес, по которому выполнен переход
+        path = environ['PATH_INFO']
+
+        my_query = parse_qsl(environ['QUERY_STRING'])
+
+        # добавление закрывающего слеша
+        if not path.endswith('/'):
+            path = f'{path}/'
+
+        # находим нужный контроллер
+        # отработка паттерна page controller
+        if path in self.routes_lst:
+            view = self.routes_lst[path]
+        else:
+            view = PageNotFound()
+        request = {}
+        # наполняем словарь request элементами
+        # этот словарь получат все контроллеры
+        # отработка паттерна front controller
+        for front in self.fronts_lst:
+            front(request)
+        for param in my_query:
+            request[param[0]] = param[1]
+        # запуск контроллера с передачей объекта request
+        code, body = view(request)
+        start_response(code, [('Content-Type', 'text/html')])
+        return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = quopri.decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
