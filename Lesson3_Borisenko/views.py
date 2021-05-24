0963044eb9e3edc062f11ee211b8cf4ac957c06f
desexcile <34170928+desexcile@users.noreply.github.com>
2021-05-24 09:25:01 +0300

    Added Lesson_3

---
+++ Lesson3_Borisenko/views.py
@@ -0,0 +1,33 @@
+from excile_framework.templator import render
+
+
+class Index:
+    def __call__(self, request):
+        return '200 OK', render('index.html', date=request.get('date', None),
+                                python_ver=request.get('python_ver', None),
+                                btc_to_usd=request.get('btc_to_usd', None))
+
+
+class About:
+    def __call__(self, request):
+        return '200 OK', render('about.html', date=request.get('date', None))
+
+
+class Bitcoin:
+    def __call__(self, request):
+        return '200 OK', 'Bitcoin'
+
+
+class Ethereum:
+    def __call__(self, request):
+        return '200 OK', 'Ethereum'
+
+
+class Hello:
+    def __call__(self, request):
+        return '200 OK', render('hello.html', username=request.get('request_params', {}).get('username'))
+
+
+class NotFound404:
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE Not Found'
